82e6215599ae83380af72a63e4e0a4a8f54bebd4
David <100006394.joan23@fje.edu>
2022-02-07 18:30:20 +0100

    cambiado el registrar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -62,6 +62,19 @@
           <div class="register" id="content_regis">
             <form action="{{url('')}}" method="POST">
               @csrf
+                <h1>Iniciar sesion en Glovo</h1><br>
+                <span><i class="fas fa-envelope"></i></span>
+                <input class="inp_txt" type="text" name="correo_user" placeholder="Introduce tu correo"><br><br>
+                <span><i class="fas fa-lock"></i></span>
+                <input type="password" name="pass_user" placeholder="Introduce tu contraseña">
+                <br><br>
+                <button class="btn_regis" type="submit" value="register">Iniciar Sesión</button><br><br>
+              </form>
+              <p>¿No tienes una cuenta todavía? <button class="btn_mostrar" onclick="mostrarlog();" id="btn_regis">Registrarme</button></p>
+          </div>
+          <div class="register2" id="content_regis2">
+            <form action="{{url('')}}" method="POST">
+              @csrf
                 <h1>Regístrate en Glovo</h1><br>
                 <span><i class="fas fa-user"></i></span>
                 <input class="inp_txt" type="text" name="correo_user" placeholder="Introduce tu nombre"><br><br>
@@ -72,20 +85,7 @@
                 <br><br>
                 <button class="btn_regis" type="submit" value="register">Registrarme</button><br><br>
               </form>
-              <p>¿Ya tienes una cuenta? <button class="btn_mostrar" onclick="mostrarlog();" id="btn_regis">Inicia sesión</button></p>
-          </div>
-          <div class="register2" id="content_regis2">
-            <form action="{{url('')}}" method="POST">
-              @csrf
-                <h1>Iniciar sesion en Glovo</h1><br>
-                <span><i class="fas fa-envelope"></i></span>
-                <input class="inp_txt" type="text" name="correo_user" placeholder="Introduce tu correo"><br><br>
-                <span><i class="fas fa-lock"></i></span>
-                <input type="password" name="pass_user" placeholder="Introduce tu contraseña">
-                <br><br>
-                <button class="btn_regis" type="submit" value="register">Iniciar Sesión</button><br><br>
-              </form>
-              <p>¿No tienes una cuenta todavía? <button class="btn_mostrar" onclick="mostrarreg();" id="btn_regis">Registrarme</button></p>
+              <p>¿Ya tienes una cuenta? <button class="btn_mostrar" onclick="mostrarreg();" id="btn_regis">Inicia sesión</button></p>
           </div>
       </div>
     </div>
